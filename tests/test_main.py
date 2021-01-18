3700f49666fefcaaf932f2b71657776f58151ac7
dhivahari-vivek-cdot <dhivahari.vivekanandasarma@state.co.us>
2021-01-17 20:43:47 -0700

    edited main func in main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -81,7 +81,7 @@ def test_help_warehouse(client, publish_client):
     print("test help-warehouse: complete!")
     assert True
 
-"""
+
 def test_main():
 
     print("test main (from main.py): begin")
@@ -92,6 +92,6 @@ def test_main():
 
     print("test main (from main.py): complete!")
     assert True
-"""
+
 
 
