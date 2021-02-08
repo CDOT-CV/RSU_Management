3cf8ec0aa9200c8ced11be6b533db8f172401d2b
dhivahari-vivek-cdot <76454486+dhivahari-vivek-cdot@users.noreply.github.com>
2021-02-08 10:12:37 -0700

    try catches

--- GCP_cloud_functions/lake-to-data-warehouse/lake-to-data-warehouse.py
+++ GCP_cloud_functions/lake-to-data-warehouse/lake-to-data-warehouse.py
@@ -17,30 +17,25 @@ def rsu_data_warehouse_bucket(event, context):
     publisher = pubsub_v1.PublisherClient()
     client = storage.Client()
 
-    try:
-        # logging cloud function trigger
-        current_time = datetime.datetime.now()
-        log_message = Template('Cloud Function "lake-to-data-warehouse" was triggered at $time')
-        logging.info(log_message.safe_substitute(time=current_time))
+    # logging cloud function trigger
+    current_time = datetime.datetime.now()
+    log_message = Template('Cloud Function "lake-to-data-warehouse" was triggered at $time')
+    logging.info(log_message.safe_substitute(time=current_time))
         
-        try:
-            topic = publisher.topic_path(config.config_vars['project_id'], config.config_vars['data_warehouse_id'])
-            blob = client.get_bucket(event['bucket']).get_blob(event['name'])
-            data_string = blob.download_as_bytes()
-            future = publisher.publish(topic,data_string)
-            print(future.result())
+    try:
+        topic = publisher.topic_path(config.config_vars['project_id'], config.config_vars['data_warehouse_id'])
+        blob = client.get_bucket(event['bucket']).get_blob(event['name'])
+        data_string = blob.download_as_bytes()
+        future = publisher.publish(topic,data_string)
+        print(future.result())
             
-            # logging publication message
-            current_time = datetime.datetime.utcnow()    
-            log_message = Template('Published message to the warehouse pub/sub topic at $time')
-            logging.info(log_message.safe_substitute(time=current_time))
+        # logging publication message
+        current_time = datetime.datetime.utcnow()    
+        log_message = Template('Published message to the warehouse pub/sub topic at $time')
+        logging.info(log_message.safe_substitute(time=current_time))
 
-        except Exception as error:
-            log_message = Template('Failed to perform actions with data warehouse Pub/Sub topic due to $message')
-            logging.error(log_message.safe_substitute(message=error))
-    
     except Exception as error:
-         log_message = Template('$error').substitute(error=error)
-         logging.error(log_message)
+        log_message = Template('Failed to perform actions with data warehouse Pub/Sub topic due to $message')
+        logging.error(log_message.safe_substitute(message=error))
 
     
