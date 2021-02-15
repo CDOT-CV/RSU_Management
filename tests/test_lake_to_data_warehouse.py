190f6e39ad01814bc65d1fac52eb0fd012063eec
dhivahari-vivek-cdot <76454486+dhivahari-vivek-cdot@users.noreply.github.com>
2021-02-15 13:07:33 -0700

    removed unused imports

--- tests/test_lake_to_data_warehouse.py
+++ tests/test_lake_to_data_warehouse.py
@@ -1,7 +1,5 @@
 from GCP_cloud_functions.lake_to_data_warehouse import lake_to_data_warehouse
 import mock
-from unittest.mock import patch, mock_open
-from unittest import TestCase
 import pytest
 
 @mock.patch("google.cloud.pubsub_v1.PublisherClient")
