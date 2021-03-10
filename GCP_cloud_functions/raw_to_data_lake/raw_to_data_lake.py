8e7a19e287d435da520ad65158da23d7d4c6cb5a
dhivahari-vivek-cdot <76454486+dhivahari-vivek-cdot@users.noreply.github.com>
2021-03-10 01:54:02 -0700

    refactored raw_to_data_lake.py for easier testing
    added main() to use as function entry point in the GCP

--- GCP_cloud_functions/raw_to_data_lake/raw_to_data_lake.py
+++ GCP_cloud_functions/raw_to_data_lake/raw_to_data_lake.py
@@ -1,6 +1,6 @@
-from GCP_cloud_functions import config
 from google.cloud import storage
-import ndjson
+import os
+import json
 import datetime
 import logging
 from string import Template
@@ -9,10 +9,10 @@ def is_json_clean(rsu_data):
     """
     Returns TRUE if json_file is clean. FALSE otherwise.
     Args: 
-        rsu_data --> RSU data as a list of dicts from raw ingest to be checked
+        rsu_data: RSU data as a list of dicts from raw ingest to be checked
     """
     isJSONclean = True
-
+    
     #check: duplicate records
     isDuplicateFree = True
     unique = []
@@ -36,16 +36,15 @@ def is_json_clean(rsu_data):
 
     return isJSONclean
 
-def raw_to_data_lake(event, context):
+def raw_to_data_lake(raw_bucket, lake_bucket,blob):
     """Triggered by new upload to the raw ingest storage bucket.
     Function retrieves this new upload and checks for cleanliness
     before sending clean data to the data lake bucket.
     Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
+         r_bucket: Raw ingest GCS bucket
+         l_bucket: Data lake GCS bucket
+         blob: Newest blob sent to raw ingest
     """
-
-    client = storage.Client()
     
     # logging cloud function trigger
     current_time = datetime.datetime.now()
@@ -53,11 +52,8 @@ def raw_to_data_lake(event, context):
     logging.info(log_message.safe_substitute(time=current_time))
         
     try:
-        raw_bucket = client.get_bucket(config.config_vars['raw_ingest_id'])           
-        lake_bucket = client.get_bucket(config.config_vars['data_lake_id'])
-        blob = raw_bucket.get_blob(event['name'])
         data_string = blob.download_as_bytes()
-        json_data = ndjson.loads(data_string)
+        json_data = json.loads(data_string)
         if is_json_clean(json_data) is True:
             raw_bucket.copy_blob(blob, lake_bucket)
             current_time = datetime.datetime.now()
@@ -67,5 +63,17 @@ def raw_to_data_lake(event, context):
     except Exception as error:
         log_message = Template('Failed to perform operations on raw and/or data lake storage buckets due to $message')
         logging.error(log_message.safe_substitute(message=error))
+
+def main(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    client = storage.Client()
+    r_bucket = client.get_bucket(os.environ['raw_ingest_id'])
+    l_bucket = client.get_bucket(os.environ['data_lake_id'])
+    raw_blob = r_bucket.get_blob(event['name'])
+    raw_to_data_lake(r_bucket,l_bucket,raw_blob)
   
 
