aef36fefa81c0cd2c42f902f535ab887034c7122
dhivahari-vivek-cdot <76454486+dhivahari-vivek-cdot@users.noreply.github.com>
2021-03-10 23:27:16 -0700

    refactored GCP functions

--- GCP_cloud_functions/lake_to_data_warehouse/lake_to_data_warehouse.py
+++ GCP_cloud_functions/lake_to_data_warehouse/lake_to_data_warehouse.py
@@ -1,21 +1,16 @@
-from GCP_cloud_functions import config
 from google.cloud import storage 
 from google.cloud import pubsub_v1
 import datetime
 import logging
+import os
 from string import Template
 
-def rsu_data_warehouse_bucket(event, context):
-    """Triggered by new upload to the data lake storage bucket. 
-    Function retrieves this new upload and publishes it to the 
-    short-term warehouse Pub/Sub as a byte string.
+def rsu_data_warehouse_bucket(pubsub_topic, lake_blob):
+    """Triggered from a message on a Cloud Pub/Sub topic.
     Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
+         pubsub_topic: The pubsub topic serving as the short-term data hub.
+         lake_blob: Most recent data pushed to the data lake GCS bucket.
     """
-    
-    publisher = pubsub_v1.PublisherClient()
-    client = storage.Client()
 
     # logging cloud function trigger
     current_time = datetime.datetime.now()
@@ -23,19 +18,28 @@ def rsu_data_warehouse_bucket(event, context):
     logging.info(log_message.safe_substitute(time=current_time))
         
     try:
-        topic = publisher.topic_path(config.config_vars['project_id'], config.config_vars['data_warehouse_id'])
-        blob = client.get_bucket(event['bucket']).get_blob(event['name'])
-        data_string = blob.download_as_bytes()
-        future = publisher.publish(topic,data_string)
+        data_string = lake_blob.download_as_bytes()
+        future = publisher.publish(pubsub_topic,data_string)
         print(future.result())
             
         # logging publication message
         current_time = datetime.datetime.utcnow()    
-        log_message = Template('Published message to the warehouse pub/sub topic at $time')
+        log_message = Template('Published message to the data hub pub/sub topic at $time')
         logging.info(log_message.safe_substitute(time=current_time))
 
     except Exception as error:
         log_message = Template('Failed to perform actions with data warehouse Pub/Sub topic due to $message')
         logging.error(log_message.safe_substitute(message=error))
-
     
+def main(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    publisher = pubsub_v1.PublisherClient()
+    client = storage.Client()
+
+    topic = publisher.topic_path(os.environ['project_id'], os.environ['data_hub_id'])
+    blob = client.get_bucket(event['bucket']).get_blob(event['name'])
+    rsu_data_warehouse_bucket(topic, blob)
