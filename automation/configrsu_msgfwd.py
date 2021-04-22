84df71cb14306d6ffdaad4702a0e8e9c5b1f70eb
drewjj <drewjj901@gmail.com>
2021-04-22 12:57:20 -0600

    includes unit tests

--- automation/configrsu_msgfwd.py
+++ automation/configrsu_msgfwd.py
@@ -1,13 +1,14 @@
 import argparse
 import csv
 import os
+import sys
 
 os.environ['SNMP_USERNAME'] = 'authOnlyUser'
 os.environ['SNMP_PASSWORD'] = 'herbertherbert'
 
 snmp_authstring = '-u {user} -a SHA -A {pw} -x AES -X {pw} -l authNoPriv'.format(user=os.getenv('SNMP_USERNAME'), pw=os.getenv('SNMP_PASSWORD'))
 
-def ipToHex(ip, little_endian):
+def ip_to_hex(ip, little_endian):
   hex_dest_ip = ''
   for octet in ip.split('.'):
     if len(hex(int(octet))[2:]) == 1:
@@ -18,7 +19,7 @@ def ipToHex(ip, little_endian):
     return '00000000000000000000ffff' + hex_dest_ip
   return hex_dest_ip + '000000000000000000000000'
 
-def setRsuStatus(rsu_ip, operate):
+def set_rsu_status(rsu_ip, operate):
   if operate:
     os.system('snmpset -v 3 {auth} {rsuip} RSU-MIB:rsuMode.0 i 4'.format(auth=snmp_authstring, rsuip=rsu_ip))
   else:
@@ -26,10 +27,10 @@ def setRsuStatus(rsu_ip, operate):
 
 def config_msgfwd(rsu_ip, dest_ip, udp_port, rsu_index):
   # Put RSU in standby
-  setRsuStatus(rsu_ip, operate=False)
+  set_rsu_status(rsu_ip, operate=False)
   
   # Create a little endian hex version of destIP
-  hex_dest_ip = ipToHex(dest_ip, little_endian=True)
+  hex_dest_ip = ip_to_hex(dest_ip, little_endian=True)
   
   # Perform configurations
   os.system('snmpset -v 3 {auth} {rsuip} RSU-MIB:rsuDsrcFwdPsid.{index} s " "'.format(auth=snmp_authstring, rsuip=rsu_ip, index=rsu_index))
@@ -43,32 +44,16 @@ def config_msgfwd(rsu_ip, dest_ip, udp_port, rsu_index):
   os.system('snmpset -v 3 {auth} {rsuip} RSU-MIB:rsuDsrcFwdEnable.{index} i 1'.format(auth=snmp_authstring, rsuip=rsu_ip, index=rsu_index))
 
   # Put RSU in run mode
-  setRsuStatus(rsu_ip, operate=True)
+  set_rsu_status(rsu_ip, operate=True)
   
   os.system('snmpwalk -v 3 -u {auth} {rsuip} 1.0.15628.4.1 | grep 4.1.7'.format(auth=snmp_authstring, rsuip=rsu_ip))
 
+def main(rsu_csv, dest_ip, udp_port, rsu_index):
+  for row in rsu_csv: 
+    config_msgfwd(row[0], str(dest_ip), str(udp_port), str(rsu_index))
+
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description=__doc__)
-  parser.add_argument(
-    "--rsu_list_file",
-	type=str,
-    required=True,
-    help="The CSV file filepath for the list of RSU IPs which will be configured.")
-  parser.add_argument(
-    "--dest_ip",
-    required=True,
-    help="The IP the RSU will forward messages to.")
-  parser.add_argument(
-    "--udp_port",
-    required=True,
-    help="The port the RSU will forward messages to.")
-  parser.add_argument(
-    "--rsu_index",
-    required=True,
-    help="The index the SNMP configuration will be configured on.")
-  args = parser.parse_args()
-  
-  with open(args.rsu_list_file, newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-      config_msgfwd(row[0], args.dest_ip, args.udp_port, args.rsu_index)
+  with open(sys.argv[1], newline='') as csvfile:
+    doc = csv.reader(csvfile, delimiter=',')
+    main(doc, sys.argv[2], sys.argv[3], sys.argv[4])
+    
